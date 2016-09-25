ec1ff0a2dbaa441b4f8f874261496ed5d4987264
harshtutsy <harshtutsy@gmail.com>
2016-09-25 12:11:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,39 +26,41 @@ var content= {
 
 
 
+function createtemplate(data){
+    var title= data.title;
+    var heading= data.heading;
+    var date= data.date;
+    var content= data.content;
+    var htmltemplate= `
+        <HTML>
+            <head>
+                <Title>
+                    ${title}
+                </Title>
+                <meta name="viewport" content="width=device-width, intial-scale=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <Body>
+                <div class="container">
+                    <div>
+                        <a href="/"> home </a>
+                    </div>
+                    <hr/>
+                    <h3>
+                        ${heading} 
+                    
+                    </h3>
+                    <div> ${date} </div>
+                    <div>
+                        ${content}
+                    </div>
+                </div>
+            </Body>
+        </HTML>
+        `;
+    
+}
 
-var htmltemplate= `
-<HTML>
-    <head>
-        <Title>
-            ${title}
-        </Title>
-        <meta name="viewport" content="width=device-width, intial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <Body>
-        <div class="container">
-            <div>
-                <a href="/"> home </a>
-            </div>
-            <hr/>
-            <h3>
-                ${heading} 
-            
-            </h3>
-            <div> ${date} </div>
-            <div>
-                ${content}
-            </div>
-        </div>
-    </Body>
-</HTML>
-
-
-
-
-
-`;
 
 
 
