283e091d480f482e7d31571e9377888c3214068c
harshtutsy <harshtutsy@gmail.com>
2016-09-25 23:47:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,3 +41,32 @@ button.onclick = function() {
     request.open('GET', 'http://harshtutsy.imad.hasura-app.io/counter' , true );
     request.send(null);
 };
+
+
+//submit name
+var nameinput = document.getElementById("name");
+var name = nameinput.value;
+var submit= document.getElementById("submit_btn");
+submit.onclick = function() {
+    //Make request to server and send name
+    //capture list of name and render it as a list
+    var names = ['name1', 'name2', 'name3'];
+    var list = '';
+    for (var i=0; i<names.length; i++){
+        list += '<li>' names[i] + '<li>'; 
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+}
+
+
+
+
+
+
+
+
+
+
+
+
