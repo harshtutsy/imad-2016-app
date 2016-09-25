19ee466a5f00e5f6b883fff9e066a3b23b4ab4b6
harshtutsy <harshtutsy@gmail.com>
2016-09-25 22:28:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,4 @@
-console.log('Loaded!');
-//change text of main-text
-var element=document.getElementById("main-text");
-element.innerHTML= "New Value";
 
-//MAking the image move when clicked on it
-var img= document.getElementById("madi")
-var marginleft= 0;
-function moveRight(){
-    marginleft=marginleft + 3;
-    img.style.marginLeft= marginleft +"px";
-    
-}
-img.onclick = function () {
-  //Every 100mili-second, apply moveright  function
-  var interval= setInterval(moveRight, 50);
-};
 
 
 var button = document.getElementById('counter');
