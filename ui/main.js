7a9bb90f7fb96dc79fdb81b72e6ac550e09433f5
harshtutsy <harshtutsy@gmail.com>
2016-09-25 22:45:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,43 @@
+//console.log('Loaded!');
+//change text of main-text
+//var element=document.getElementById("main-text");
+//element.innerHTML= "New Value";
 
+//MAking the image move when clicked on it
+//var img= document.getElementById("madi")
+//var marginleft= 0;
+//function moveRight(){
+  //  marginleft=marginleft + 3;
+    //img.style.marginLeft= marginleft +"px";
+    
+//}
+//img.onclick = function () {
+  //Every 100mili-second, apply moveright  function
+ // var interval= setInterval(moveRight, 50);
+// };
 
 
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function() {
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and set it in the variable
+    
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    };
     
     
-    //render the variable in correct span.
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
-    
+    request.open('GET', 'http://harshtutsy.imad.hasura-app.io/counter' , true );
+    request.send(null);
 };
