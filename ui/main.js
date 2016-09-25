d185c47783fcb394a46e1ff4d9226e1f92cb9593
harshtutsy <harshtutsy@gmail.com>
2016-09-26 01:00:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,8 +57,8 @@ submit.onclick = function() {
     request.onreadystatechange = function() {
         if(request.readyState == XMLHttpRequest.DONE) {
             //Take some action
-            if(request.status == 200){
-                var names = response.requestText;
+            if(request.status === 200){
+                var names = request.responseText;
                 names= JSON.parse(names);
                 var list = '';
                 for (var i=0; i < names.length; i++) {
