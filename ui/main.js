92224733953d83a48950bcb36216cf81f8383115
harshtutsy <harshtutsy@gmail.com>
2016-09-25 19:20:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change text of main-text
 var element=document.getElementById("main-text");
 element.innerHTML= "New Value";
+
+//MAking the image move when clicked on it
+var img= document.getElementById("madi")
+img.onclick = function () {
+  img.style.marginLeft= '100px';
+  
+}; 
