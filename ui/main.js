2428bfe89d3f8150b72e8ec646dd424faa0bacf4
harshtutsy <harshtutsy@gmail.com>
2016-09-25 19:27:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,13 @@ element.innerHTML= "New Value";
 
 //MAking the image move when clicked on it
 var img= document.getElementById("madi")
+var marginleft= 0;
+function moveRight(){
+    marginleft=marginleft + 10;
+    img.style.marginLeft= marginleft +"px";
+    
+}
 img.onclick = function () {
-  img.style.marginLeft= '100px';
-  
+  //Every 100mili-second, apply moveright  function
+  var interval= setInterval(moveRight, 100);
 }; 
